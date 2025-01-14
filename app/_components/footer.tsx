50fa8b38c202d41e5ba818ddaf2e62ca00508ac6
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-01-14 14:16:18 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-foreground px-5 py-6">
+      <p className="text-xs text-accent">
+        © 2025 Copyright <span className="font-bold">FSW Barber</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import NextAuthProvider from "./_providers/auth";
+import Footer from "./_components/footer";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${nunito.className} antialiased`}>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <NextAuthProvider>
+          {children}
+          <Footer />
+        </NextAuthProvider>
       </body>
     </html>
   );
