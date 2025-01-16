dbc477a68e008a24528af036ad72ad3464769a11
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-01-16 15:31:59 -0300

    feat: ensure home page can be returned when button is clicked

--- app/barbershops/[id]/_components/image-barbershop.tsx
+++ app/barbershops/[id]/_components/image-barbershop.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/app/_components/ui/button";
 import { BarbershopDTO } from "@/app/_data/get-barbershop";
 import { ChevronLeft, X } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const ImageBarbershop = ({ barbershop }: { barbershop: BarbershopDTO }) => {
+  const router = useRouter();
   return (
     <div className="relative h-[250px] w-full">
       <Image
@@ -15,12 +19,14 @@ const ImageBarbershop = ({ barbershop }: { barbershop: BarbershopDTO }) => {
       <Button
         className="absolute left-4 top-4 bg-foreground text-white hover:bg-muted-foreground"
         size="icon"
+        onClick={() => router.back()}
       >
         <ChevronLeft size={24} />
       </Button>
       <Button
         className="absolute right-4 top-4 bg-foreground text-white hover:bg-muted-foreground"
         size="icon"
+        onClick={() => router.push("/")}
       >
         <X size={24} />
       </Button>
