0751667e174e7f9c5c6cb856978ceaba1325eee5
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-01-20 19:30:22 -0300

    feat: add initial booking service component

--- app/barbershops/[id]/_components/service-barbershop-item.tsx
+++ app/barbershops/[id]/_components/service-barbershop-item.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { BarbershopServiceDTO } from "@/app/_data/get-barbershop";
 import Image from "next/image";
+import BookingService from "./booking-service";
 
 const ServiceBarbershopItem = ({
   service,
@@ -8,29 +10,38 @@ const ServiceBarbershopItem = ({
   service: BarbershopServiceDTO;
 }) => {
   return (
-    <div className="grid h-[134px] w-full grid-cols-3 gap-3 rounded-xl border border-muted bg-foreground p-3">
-      <div className="col-span-1">
-        <Image
-          src={service.imageUrl}
-          alt={service.name}
-          width={110}
-          height={110}
-          className="rounded-lg object-cover"
-        />
-      </div>
-      <div className="col-span-2">
-        <div>
-          <h2 className="text-sm font-bold text-white">{service.name}</h2>
-          <p className="text-sm text-accent">{service.description}</p>
+    <Sheet>
+      <div className="grid h-[134px] w-full grid-cols-3 gap-3 rounded-xl border border-muted bg-foreground p-3">
+        <div className="col-span-1">
+          <Image
+            src={service.imageUrl}
+            alt={service.name}
+            width={110}
+            height={110}
+            className="rounded-lg object-cover"
+          />
         </div>
-        <div className="flex items-center justify-between gap-7">
-          <h3 className="text-sm font-bold text-primary">R${service.price}</h3>
-          <Button className="rounded-xl bg-muted hover:bg-muted-foreground">
-            Reservar
-          </Button>
+        <div className="col-span-2">
+          <div>
+            <h2 className="text-sm font-bold text-white">{service.name}</h2>
+            <p className="text-sm text-accent">{service.description}</p>
+          </div>
+          <div className="flex items-center justify-between gap-7">
+            <h3 className="text-sm font-bold text-primary">
+              R${service.price}
+            </h3>
+            <SheetTrigger asChild>
+              <Button className="rounded-xl bg-muted hover:bg-muted-foreground">
+                Reservar
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="w-[95%] border-none text-white">
+              <BookingService />
+            </SheetContent>
+          </div>
         </div>
       </div>
-    </div>
+    </Sheet>
   );
 };
 
