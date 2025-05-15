7fd692a16864e1c3c63be0ba1ebc902a7e888151
Ryan Oliveira <ryanprofissional02@gmail.com>
2025-05-15 12:28:01 -0300

    fix: ensure Toaster component is used

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
         <NextAuthProvider>
           {children}
           <Footer />
-        </NextAuthProvider>
         <Toaster />
+        </NextAuthProvider>
       </body>
     </html>
   );
